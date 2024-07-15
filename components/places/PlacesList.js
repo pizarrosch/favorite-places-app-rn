3ecbc1f6af2afec1dfb7dc978d3e5042d56b443c
pizarrosch <bayenrshc89@gmail.com>
2024-07-15 19:58:49 +0200

    Create all necessary screen and component files, create a FlatList, which will contain a list of places in the future

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,7 +1,12 @@
 import {FlatList} from "react-native";
+import PlaceItem from "../PlaceItem";
 
 function PlacesList({places}) {
-  return <FlatList data={places} renderItem={} />
+  return <FlatList
+    data={places}
+    keyExtractor={item => item.id}
+    renderItem={({item}) => <PlaceItem place={item}/>}
+  />
 }
 
 export default PlacesList;
---
+++ models/Place.js
@@ -0,0 +1,9 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; //{latitude: 0.123434, longitude: 134.49}
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}

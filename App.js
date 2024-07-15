aaf98a3643228696cb44c9176b78317a28c63543
pizarrosch <bayenrshc89@gmail.com>
2024-07-15 23:13:57 +0200

    Add an add button in the header using Ionicons and add some styling and onPress function

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-   <>
-    <StatusBar />
-     <NavigationContainer>
-       <Stack.Navigator>
-         <Stack.Screen name='AllPlaces' component={AllPlaces} />
-         <Stack.Screen name='AddPlace' component={AddPlace} />
-       </Stack.Navigator>
-     </NavigationContainer>
-   </>
+    <>
+      <StatusBar/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='AllPlaces' component={AllPlaces} options={
+            {headerRight: ({tintColor}) => <IconButton icon='add' color={tintColor} size={28}/>}
+          }/>
+          <Stack.Screen name='AddPlace' component={AddPlace}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 

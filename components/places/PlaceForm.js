6f3bb288fcdaa20034979ec5278e328802299e4e
pizarrosch <bayenrshc89@gmail.com>
2024-07-16 00:24:03 +0200

    Create an input form and give it some styles

--- components/places/PlaceForm.js
+++ components/places/PlaceForm.js
@@ -1,9 +1,43 @@
-import {View, Text} from "react-native";
+import {View, Text, ScrollView, TextInput, StyleSheet} from "react-native";
+import {useState} from "react";
+import {Colors} from "../../constants/Colors";
 
-export default function PlaceForm() {
+export default function PlaceForm({label}) {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  function changeTitleHandler(enteredText) {
+    setEnteredTitle(enteredText);
+  }
+
   return (
     <View>
-      <Text>Place form</Text>
+      <ScrollView style={styles.form}>
+        <View>
+          <Text style={styles.label}>{label}</Text>
+          <TextInput onChangeText={changeTitleHandler} value={enteredTitle} style={styles.input}/>
+        </View>
+      </ScrollView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 24
+  },
+  input: {
+    backgroundColor: Colors.primary100,
+    borderRadius: 6,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginTop: 10,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2
+  },
+  label: {
+    color: Colors.primary50,
+    fontWeight: "bold"
+  }
+})
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,7 +1,7 @@
 import PlaceForm from "../components/places/PlaceForm";
 
 function AddPlace() {
-  return <PlaceForm />
+  return <PlaceForm label='Type the place name'/>
 }
 
 export default AddPlace;

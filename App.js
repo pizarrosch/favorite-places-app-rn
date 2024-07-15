a2f444f0af553a844a20f0e022f2da16f8b64932
pizarrosch <bayenrshc89@gmail.com>
2024-07-15 23:23:45 +0200

    Let the onPress function navigate to the AddPlace screen, using the inline function in the options object

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from "@react-navigation/native";
+import {NavigationContainer, useNavigation} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
@@ -9,14 +9,29 @@ import IconButton from "./components/UI/IconButton";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  // const navigation = useNavigation();
+
   return (
     <>
       <StatusBar/>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='AllPlaces' component={AllPlaces} options={
-            {headerRight: ({tintColor}) => <IconButton icon='add' color={tintColor} size={28}/>}
-          }/>
+          <Stack.Screen
+            name='AllPlaces'
+            component={AllPlaces}
+            options={({navigation}) =>
+              ({
+                headerRight: ({tintColor}) => <
+                  IconButton
+                  icon='add'
+                  color={tintColor}
+                  size={28}
+                  onPress={() => navigation.navigate('AddPlace')}
+                />
+              })
+            }
+          />
           <Stack.Screen name='AddPlace' component={AddPlace}/>
         </Stack.Navigator>
       </NavigationContainer>

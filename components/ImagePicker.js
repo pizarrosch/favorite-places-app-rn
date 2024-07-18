f535908a69103ce0efc251f4d0bfa96e93707c30
pizarrosch <bayenrshc89@gmail.com>
2024-07-18 18:17:09 +0200

    Create a custom outline button

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,8 +1,12 @@
-import {Alert, Button, View} from "react-native";
+import {Alert, Button, View, Image, Text, StyleSheet} from "react-native";
 import {launchCameraAsync, useCameraPermissions, PermissionStatus} from "expo-image-picker";
+import {useState} from "react";
+import {Colors} from "../constants/Colors";
+import OutlineButton from "./UI/OutlineButton";
 
 export default function ImagePicker() {
   const [cameraPermissionStatus, requestPermission] = useCameraPermissions();
+  const [takenPhoto, setTakenPhoto] = useState('');
 
   async function verifyPermissions() {
     if (cameraPermissionStatus.status === PermissionStatus.UNDETERMINED) {
@@ -19,22 +23,49 @@ export default function ImagePicker() {
   async function takePhotoHandler() {
     const isPermissionGranted = await verifyPermissions();
 
-    if (!isPermissionGranted) return;
+    // if (!isPermissionGranted) return;
 
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5
     });
-    console.log(image);
+    const imageURI = image.assets.map(image => image.uri).toString();
+    setTakenPhoto(imageURI);
+    console.log(takenPhoto)
+  }
+
+  let imagePreview = <Text style={styles.fallbackText}>No image taken yet</Text>
+
+  if (takenPhoto) {
+    imagePreview = <Image source={{uri: takenPhoto}} style={styles.image}/>
   }
 
   return (
     <View>
-      <View>
-
+      <View style={styles.imageContainer}>
+        {imagePreview}
       </View>
-      <Button title="Take photo" onPress={takePhotoHandler}/>
+      <OutlineButton icon='camera' onPress={takePhotoHandler}>Take Photo</OutlineButton>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: '100%',
+    height: 200,
+    marginVertical: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    backgroundColor: Colors.primary100
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  fallbackText: {
+    fontSize: 16
+  }
+})

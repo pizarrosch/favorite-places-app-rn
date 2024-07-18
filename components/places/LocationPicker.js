51fc5c2a77eb7f4a987156c199ddda3a1f5c72fd
pizarrosch <bayenrshc89@gmail.com>
2024-07-18 18:45:22 +0200

    Create a function to get actual coordinates, with the verification function allowing to use location

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,11 +1,27 @@
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, Alert} from "react-native";
 import OutlinedButton from "../UI/OutlinedButton";
 import {Colors} from "../../constants/Colors";
+import {getCurrentPositionAsync, useForegroundPermissions, PermissionStatus} from "expo-location";
 
 export default function LocationPicker() {
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+  async function verifyPermission() {
 
-  function getLocationHandler() {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
 
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert('Permission is missing', 'Please go to settings and allow your device to use your location');
+      return false;
+    }
+  }
+
+  async function getLocationHandler() {
+    await verifyPermission();
+    const location = await getCurrentPositionAsync();
+    console.log(location);
   }
 
   function pickOnMapHandler() {

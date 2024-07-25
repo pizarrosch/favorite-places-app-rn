c2e0da795781400805b3bf4040ebe2296d8b836e
pizarrosch <bayenrshc89@gmail.com>
2024-07-25 20:22:59 +0200

    Create a save button in the header of a map view screen and go back to the 'Add A New Place' screen when tapping on it (if a place is marked)

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,9 @@
 import MapView, {Marker} from "react-native-maps";
-import {View, StyleSheet} from "react-native";
-import {useState} from "react";
+import {View, StyleSheet, Alert} from "react-native";
+import {useState, useLayoutEffect, useCallback} from "react";
+import IconButton from "../components/UI/IconButton";
 
-export default function Map() {
+export default function Map({navigation}) {
 
   const [selectedLocation, setSelectedLocation] = useState(null);
 
@@ -23,21 +24,37 @@ export default function Map() {
     })
   }
 
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert('No location picked', 'Tap on the map to pick your location');
+      return;
+    }
+
+    navigation.navigate('AddPlace', {
+      pickedLat: selectedLocation?.lat,
+      pickedLong: selectedLocation?.long
+    })
+  }, [navigation, selectedLocation])
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({tintColor}) => <IconButton icon='save' size={24} color={tintColor} onPress={savePickedLocationHandler} />
+    });
+  }, [navigation, savePickedLocationHandler])
+
   return (
     <View style={styles.mapContainer}>
       <MapView
         style={styles.map}
         initialRegion={region}
         onPress={selectLocationHandler}>
-        {selectedLocation &&
-          (<Marker
-              title='Picked Location'
-              coordinate={{
-                latitude: selectedLocation.lat,
-                longitude: selectedLocation.long
-              }}
-            />
-          )}
+        <Marker
+          title='Picked Location'
+          coordinate={{
+            latitude: selectedLocation?.lat,
+            longitude: selectedLocation?.long
+          }}
+        />
       </MapView>
     </View>
   )

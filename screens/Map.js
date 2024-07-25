f40e03fef5ae2f621befbade7041bdb74b2cf6da
pizarrosch <bayenrshc89@gmail.com>
2024-07-25 19:53:21 +0200

    Set location marker on the map view

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,11 @@
 import MapView, {Marker} from "react-native-maps";
 import {View, StyleSheet} from "react-native";
+import {useState} from "react";
 
 export default function Map() {
+
+  const [selectedLocation, setSelectedLocation] = useState(null);
+
   const region = {
     latitude: 34.9496,
     longitude: -81.9317,
@@ -9,9 +13,32 @@ export default function Map() {
     longitudeDelta: 0.08
   }
 
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const long = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({
+      lat: lat,
+      long: long
+    })
+  }
+
   return (
     <View style={styles.mapContainer}>
-      <MapView style={styles.map} initialRegion={region}></MapView>
+      <MapView
+        style={styles.map}
+        initialRegion={region}
+        onPress={selectLocationHandler}>
+        {selectedLocation &&
+          (<Marker
+              title='Picked Location'
+              coordinate={{
+                latitude: selectedLocation.lat,
+                longitude: selectedLocation.long
+              }}
+            />
+          )}
+      </MapView>
     </View>
   )
 }
